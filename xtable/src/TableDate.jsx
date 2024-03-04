8a8c0fba81c590d2ccf094951758c16f09fd259b
KAARTHIIK M <kaarthiikmr001@gmail.com>
2024-03-04 20:39:41 +0530

    sorting date data in table

---
+++ xtable/src/TableDate.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const TableDate = () => {
+  return (
+    <div>
+        <h1>Date and Views Table</h1>
+        <button>Sort by Date</button>
+        <button>Sort by Views</button>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Views</th>
+                    <th>Article</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TableDate
